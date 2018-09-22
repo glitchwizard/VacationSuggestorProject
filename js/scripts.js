6053f592dbedd6300b27cb4492d8eeaa54ee17ca
glitchwizard <glitchwizard@gmail.com>
2018-09-22 11:45:06 -0700

    This commet has everything working as intended, but no embellishments.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,40 @@
-$(document).ready(event) {
-    
-}
+$(document).ready(function(event) {
+    $("#formBlanks form").submit(function(event) {
+        var weatherInput = parseInt($("#weather").val());
+        var environmentInput = parseInt($("#environment").val());
+        var moodInput = parseInt($("#mood").val());
+        var distanceInput = parseInt($("#distance").val());
+        var foodInput = parseInt($("#food").val());
+
+        console.log("This is weather input: " + weatherInput);
+        console.log("This is environment input: " + environmentInput);
+        console.log("This is mood input: " + moodInput);
+        console.log("This is distance input: " + distanceInput);
+        console.log("This is food input: " + foodInput);
+
+        var sumTotal = weatherInput + environmentInput + moodInput + distanceInput + foodInput;
+
+        console.log("This is the total sum: " + sumTotal)
+
+        if (sumTotal <= 8 && sumTotal >= 5) {
+            $("#Results1").show();
+            $("#Answer").show();
+            $("#formBlanks").hide();
+            alert("This is option1");
+        } else if ( sumTotal > 8 && sumTotal <= 12) {
+            $("#Results2").show();
+            $("#Answer").show();
+            $("#formBlanks").hide();
+            alert("This is option2");
+        } else if ( sumTotal > 12 ) {
+            $("#Results3").show();
+            $("#Answer").show();
+            $("#formBlanks").hide();
+            alert("This is option3");
+        } else {
+            alert("You didn't select enough answers, please select them now.")
+        };
+
+        event.preventDefault();        
+    });
+});
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Vacation Selector
+
+This is a project for Epicodus Code Camp creating a logic driven, user submitted form to find a vacation location based on simple inputs
+
+## Installation
+
+Simply clone this to your machine, and open the index.html
+
+### Requirements
+
+- Git (to clone the data to your machine)
+- A normal web browser (to access index.html)
+
+## Usage
+
+Answer the questions, submit the answers, and recieve your suggestion
+
+## Development
+
+V. 1.0 - Submitted for the project
+
+## Contributing
+
+-Charley McGowan
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
